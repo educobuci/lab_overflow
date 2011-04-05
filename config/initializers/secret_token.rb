4ef3d39e0f9fdd55d18951b1f9d0ab7f03c74c5f
Eduardo Cobuci <educobuci@gmail.com>
2011-04-05 19:44:46 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabOverflow::Application.config.secret_token = '599f1a12acbf3cf8107e8a888edbcccbfe8f95476b02ddf38aa7819fce4d68c33ed1e48b0a0c105838dd4261524e2cc42c86c5ec99e5c5685ac2ef298a1e6b75'
