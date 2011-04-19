7968728780895cae351d53749cd9cbbace0e605d
Eduardo Cobuci <educobuci@gmail.com>
2011-04-19 18:56:39 -0300

    question registration feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,4 +5,10 @@ class QuestionsController < ApplicationController
     @question = Question.new
     respond_with @question
   end
+  
+  def create
+    @question = Question.new(params[:question])
+    flash[:notice] = "Success" if @question.save!
+    redirect_to root_url
+  end
 end
--- spec/acceptance/question_registration_spec.rb
+++ spec/acceptance/question_registration_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 require 'capybara/rspec'
 
 feature "question resgistration" do
-  scenario "regular resgitration" do
+  scenario "regular resgistration" do
     visit '/'
     click_link 'Ask Question'
     within '#new_question' do
       fill_in 'Title', :with => 'My question'
-      fill_in '#question_text', :with => 'This is the question test!'
+      fill_in 'question_text', :with => 'This is the question test!'
     end
     click_link_or_button 'Post your Question'
     page.should have_content 'Success'
-  end
+  end  
 end
